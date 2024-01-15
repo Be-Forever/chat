193b7b6ef18e7df4e37ff30c59bd99d805ab4e6c
GitHub Action <action@github.com>
2024-01-15 03:13:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/beforever/chat/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/beforever/chat/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.beforever.chat.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.beforever.chat.MyBundle
+import com.github.beforever.chat.services.MyProjectService
 import javax.swing.JButton
 
 
